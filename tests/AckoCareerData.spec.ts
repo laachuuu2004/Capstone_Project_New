748085faedbb51a9221c90bdc121b89f2c2dcfaa
laachuuu2004 <laachuuu.2004@gmail.com>
2026-09-23 22:21:05 +0530

    update on files

--- tests/AckoCareerData.spec.ts
+++ tests/AckoCareerData.spec.ts
@@ -21,8 +21,8 @@ test.describe("To verify that the user can successfully apply for the Senior Pro
       //await careersPage.clickOnAllLocations();
       //console.log("Location from JSON:", location);
       await careersPage.selectLocation(location);
-      
-     await careersPage.clickSeniorProductDesignerApplyNow();
+      await careersPage.clickSeniorProductDesigner();
+      await careersPage.clickApplyForThisPosition();
 
       
       console.log({ location, firstName, lastName, email, phoneNumber, title, company, currentLocation, noticePeriod, currentCTC, expectedCTC });
